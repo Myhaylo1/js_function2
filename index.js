132e5b92bd2a7ea7dcd5d602d0162f2e57c2919e
Myhaylo1 <zpkazka@gmail.com>
2020-06-30 23:24:36 +0300

    ADD NEW FUNCTION squareRhombus()

--- index.js
+++ index.js
@@ -18,9 +18,10 @@ console.log('isAdult(4)=false=', isAdult(4));
  * @returns {boolean} a%b=0
  */
 function checkMultiplicity(a, b) {
-    if (!(isNaN(a) && isNaN(b))) return (a % b) === 0;
+    if (!(isNaN(a) || isNaN(b))) return (a % b) === 0;
 }
 
+console.log('checkMultiplicity("a1",5)=undefined=', checkMultiplicity('a1', 5));
 console.log('checkMultiplicity(25,5)=true=', checkMultiplicity(25, 5));
 console.log('checkMultiplicity(15,3)=true=', checkMultiplicity(15, 3));
 console.log('checkMultiplicity(15,5)=true=', checkMultiplicity(15, 5));
@@ -35,9 +36,9 @@ console.log('checkMultiplicity(15,4)=false=', checkMultiplicity(15, 4));
  */
 function triangle(a, b, c) {
     if (!(isNaN(a) || isNaN(b) || isNaN(c))) {
-        a=Number(a);
-        b=Number(b);
-        c=Number(c);
+        a = Number(a);
+        b = Number(b);
+        c = Number(c);
         return (((a + b) > c) && ((a + c) > b) && ((c + b) > a));
     }
 }
@@ -45,3 +46,20 @@ function triangle(a, b, c) {
 console.log('triangle("a",4,5)=undefined=', triangle('a', 4, 5));
 console.log('triangle(3,4,5)=true=', triangle(3, 4, 5));
 console.log('triangle(3,4,9)=false=', triangle(3, 4, 9));
+
+function squareRhombus(i, d1, d2) {
+    if (!(isNaN(d1) || isNaN(d2))) {
+        d1 = Number(d1);
+        d2 = Number(d2);
+        switch (Number(i)) {
+            case 1:
+                return (d1 * d2 / 2);
+            case 2:
+                return (d1 * d1 * Math.sin(d2));
+        }
+    }
+}
+
+console.log('The first equation: squareRhombus(0,4,3)=undefined=', squareRhombus(0, 4, 3));
+console.log('The first equation: squareRhombus(1,2,3)=3=', squareRhombus(1, 2, 3));
+console.log('The second equation: squareRhombus(2,1,pi/2)=1=', squareRhombus(2, 1, Math.PI / 2));
