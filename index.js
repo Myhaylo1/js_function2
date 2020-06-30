1212afc5b374e210323c2fc7548d731af30de275
Myhaylo1 <zpkazka@gmail.com>
2020-06-30 19:20:05 +0300

    ADD FUNCTION triangle()

--- index.js
+++ index.js
@@ -4,11 +4,12 @@
  * @returns {boolean}  //or undefined
  */
 function isAdult(x) {
-    if (!isNaN(x))  return x >= 16;
+    if (!isNaN(x)) return x >= 16;
 }
-console.log('isAdult("sdf")=', isAdult('sdf'));
-console.log('isAdult(20)=', isAdult(20));
-console.log('isAdult(4)=', isAdult(4));
+
+console.log('isAdult("sdf")=undefined=', isAdult('sdf'));
+console.log('isAdult(20)=true=', isAdult(20));
+console.log('isAdult(4)=false=', isAdult(4));
 
 /**
  *
@@ -16,11 +17,25 @@ console.log('isAdult(4)=', isAdult(4));
  * @param {number} b
  * @returns {boolean} a%b=0
  */
-function checkMultiplicity(a,b)
-{
-  if (!(isNaN(a) && isNaN(b))) return (a%b)===0;
+function checkMultiplicity(a, b) {
+    if (!(isNaN(a) && isNaN(b))) return (a % b) === 0;
 }
-console.log('checkMultiplicity(25,5)=',checkMultiplicity(25,5));
-console.log('checkMultiplicity(15,3)=',checkMultiplicity(15,3));
-console.log('checkMultiplicity(15,5)=',checkMultiplicity(15,5));
-console.log('checkMultiplicity(15,4)=',checkMultiplicity(15,4));
+
+console.log('checkMultiplicity(25,5)=true=', checkMultiplicity(25, 5));
+console.log('checkMultiplicity(15,3)=true=', checkMultiplicity(15, 3));
+console.log('checkMultiplicity(15,5)=true=', checkMultiplicity(15, 5));
+console.log('checkMultiplicity(15,4)=false=', checkMultiplicity(15, 4));
+
+/**
+ *
+ * @param {number} a
+ * @param {number} b
+ * @param {number} c
+ * @returns {boolean} (a+b)>c
+ */
+function triangle(a, b, c) {
+    return (((a + b) > c) && ((a + c) > b) && ((c + b) > a));
+}
+
+console.log('triangle(3,4,5)=true=', triangle(3, 4, 5));
+console.log('triangle(3,4,9)=false=', triangle(3, 4, 9));
